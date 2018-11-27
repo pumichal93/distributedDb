11f5dc8d49ac4bc02903625ff89ffe0ab4e66709
Michal Puskas <michalpuskas93@gmail.com>
2018-11-27 17:16:23 +0100

    add node_id to insert

--- pridany-tovar-ok.php
+++ pridany-tovar-ok.php
@@ -1,6 +1,6 @@
 ﻿<?php
 include "query-functions.php"; 
-
+include "config.php";
 // prepare data to db insert
 $insert_data = [
 	'nazov' => $_POST["nazov"],
@@ -8,7 +8,8 @@ $insert_data = [
 	'popis' => $_POST["popis"],
 	'farba' => $_POST["farba"],
 	'cena' => $_POST["cena"],
-	'kod' => $_POST["kod"]
+	'kod' => $_POST["kod"],
+	'node_id' => $name
 ];
 
 addNewRow('tovar', $insert_data);
